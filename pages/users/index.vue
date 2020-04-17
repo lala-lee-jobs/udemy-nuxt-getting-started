8ffc4d0dac60b9d878e3688a7b6dc767cba1fe0a
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-17 22:30:01 +0800

    Creating Nested Routes

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
-    <h1>Users</h1>
-    <div>
-      <input type="text" v-model="userId" placeholder="Pleack input userId" />
-      <button @click="onLoadUser">Load User</button>
-    </div>
-    <div>
-      <nuxt-link to="/users/1">User 1 -> OK</nuxt-link>
-    </div>
-    <div>
-      <nuxt-link to="/users/a">User a -> 404</nuxt-link>
-    </div>
+    <input type="text" v-model="userId" />
+    <button @click="onLoadUser">Load User</button>
   </div>
 </template>
-
 <script>
 export default {
   data() {
