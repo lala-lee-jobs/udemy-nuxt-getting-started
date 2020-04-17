6f3f1a17beff55e175a6b3775426887bd6dd26c1
lala.lee <lala.lee.jobs@gmail.com>
2020-04-17 18:13:03 +0800

    Time to Practice - Pages & Routing

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Product {{ this.$route.params.pid }}</h1>
+  </div>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <nuxt-link to="/products/1">Product1</nuxt-link>
+    <nuxt-link to="/products/2">Product2</nuxt-link>
+  </div>
+</template>
