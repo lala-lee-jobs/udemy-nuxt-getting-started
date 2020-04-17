2ab60706d1ea2d3a56828afeddef2fa0f812b605
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-17 20:57:23 +0800

    Validating Parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,12 @@
     Display your id：{{ $route.params.id }}
   </div>
 </template>
+<script>
+export default {
+  validate({params}) {
+    // Must be a number 路由參數ID只能輸入數字
+    // 若不為數字則會自動加載顯示404錯誤頁面
+    return /^\d+$/.test(params.id);
+  },
+};
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    Users
-    <input type="text" v-model="userId" placeholder="Pleack input userId" />
-    <button @click="onLoadUser">Load User</button>
+    <h1>Users</h1>
+    <div>
+      <input type="text" v-model="userId" placeholder="Pleack input userId" />
+      <button @click="onLoadUser">Load User</button>
+    </div>
+    <div>
+      <nuxt-link to="/users/1">User 1 -> OK</nuxt-link>
+    </div>
+    <div>
+      <nuxt-link to="/users/a">User a -> 404</nuxt-link>
+    </div>
   </div>
 </template>
 
